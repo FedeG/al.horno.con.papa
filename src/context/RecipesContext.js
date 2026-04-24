8aed6ad590a05d618a23cabf4b24136a7547a10f
FedeG <federicogonzalez761@gmail.com>
2026-04-24 15:55:31 -0300

    Fix eslint errors

--- src/context/RecipesContext.js
+++ src/context/RecipesContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext } from 'react';
 import recipesData from '../data/recipes.json';
 
 const RecipesContext = createContext();
